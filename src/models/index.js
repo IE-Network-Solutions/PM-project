83ebead2b4691b4a01e311d25e5b0401669685d4
abeselomg <abeselomgebrekidan12@gmail.com>
2023-08-17 09:56:14 +0300

    reverted

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,11 @@
 
 module.exports.Risk = require('./risk.model');
 module.exports.Issue = require('./issue.model');
-
 module.exports.AfterActionAnalysis = require('./AAA.model');
 module.exports.RelatedIssue = require('./relatedIssues.model');
 module.exports.Action = require('./action.model');
 // module.exports.AfterActionAnalysisIssueRelated = require('./afterActionAnalysisIssueRelated.model');
-
 module.exports.Project = require('./project.model');
 module.exports.Milestone = require('./milestone.model');
 module.exports.Task = require('./task.model');
-
 module.exports.User =require('./user.model')
