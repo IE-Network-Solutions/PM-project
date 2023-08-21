3b0b8a0900e6175d65dbd5295925807bb9bd5e7f
abeselomg <abeselomgebrekidan12@gmail.com>
2023-08-21 11:54:59 +0300

    module name updated

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ module.exports.Action = require('./action.model');
 // module.exports.AfterActionAnalysisIssueRelated = require('./afterActionAnalysisIssueRelated.model');
 module.exports.Project = require('./project.model');
 module.exports.paymentTerm = require('./PaymentTerm.model')
-module.exports.ProjectMembers = require('./ProjectMember.model');
+module.exports.ProjectMembers = require('./projectMember.model');
 module.exports.ProjectContractValue = require('./projectContractValue.model');
 
 module.exports.Milestone = require('./milestone.model');
