642ce2c1f0c2f2b3887f79dfa03d9080ca23cec8
Kirubel25 <kirubel667@gmail.com>
2023-09-25 17:00:50 +0300

    changes made on master budget and filter budget by date

--- src/controllers/budget.controller.js
+++ src/controllers/budget.controller.js
@@ -184,6 +184,20 @@ const addBudget = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(budget);
 });
 
+const masterBudget = catchAsync(async (req, res) => {
+  const data = await budgetService.masterBudget();
+  res.send(data);
+});
+
+const filterBudget = catchAsync(async (req, res) => {
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  const data = await budgetService.filterBudget(startDate, endDate);
+
+  res.send(data);
+});
+
 module.exports = {
   createBudget,
   getBudgets,
@@ -198,4 +212,6 @@ module.exports = {
   getMonthlyBudget,
   budgetGroups,
   getBudgetsByGroup,
+  masterBudget,
+  filterBudget,
 };
