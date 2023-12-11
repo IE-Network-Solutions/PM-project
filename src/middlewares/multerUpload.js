7522d64444250b924b6b6a1f91e6a9d9fe007ceb
Ephraim solomon <efisolm22@gmail.com>
2023-12-11 16:59:39 +0300

    Upload fixed and started

--- src/middlewares/multerUpload.js
+++ src/middlewares/multerUpload.js
@@ -1,30 +1,3 @@
-// const path = require('path');
-// const multer = require('multer');
-// const { validateFile } = require('../validations/upload.validation');
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './uploads');
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   // Use the validateFile function from Joi to perform file validation
-//   const isValid = validateFile(file);
-
-//   if (isValid) {
-//     // Accept the file
-//     cb(null, true);
-//   } else {
-//     // Reject the file with an error message
-//     cb(new Error('Invalid file'), false);
-//   }
-// };
-
-// exports.uploadOptions = multer({ storage: storage});
 
 const multer = require("multer");
 
