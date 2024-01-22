6b28de79c9798c05af9d6f44914903e9ee15f9ed
Kirubel25 <kirubel667@gmail.com>
2024-01-22 12:31:58 +0300

    get all users controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,25 +4,31 @@ const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
 
-
 const getUsers = catchAsync(async (req, res) => {
-    const filter = pick(req.query, []);
-    const options = pick(req.query, ['sortBy', 'limit', 'page']);
-    const result = await userService.queryUsers(filter, options);
-    res.send(result);
+  const filter = pick(req.query, []);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await userService.queryUsers(filter, options);
+  res.send(result);
 });
 
+const getAllUsers = catchAsync(async (req, res) => {
+  const users = await userService.getUsers();
+  if (!users) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'No Users found');
+  }
+  res.send(users);
+});
 
 const getUser = catchAsync(async (req, res) => {
-    const user = await userService.getUserById(req.params.userId);
-    if (!user) {
-        throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
-    }
-    res.send(user);
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  res.send(user);
 });
 
-
 module.exports = {
-    getUsers,
-    getUser
+  getUsers,
+  getUser,
+  getAllUsers,
 };
