02dee295d3ac7fcfd5f290089246e29b23fbe5ed
abeselomg <abeselomgebrekidan12@gmail.com>
2024-02-06 10:52:51 +0300

    add milestone update

--- src/validations/milestone.validation.js
+++ src/validations/milestone.validation.js
@@ -11,8 +11,7 @@ const createMilestone = {
 };
 
 const getMilestones = {
-  query: Joi.object().keys({
-  }),
+  query: Joi.object().keys({}),
 };
 
 const getMilestone = {
@@ -31,42 +30,44 @@ const updateMilestone = {
     milestoneId: Joi.required(),
   }),
   body: Joi.object()
-    // .keys({
-    //   id: Joi.string().uuid().required(),
-    //   name: Joi.string(),
-    //   status: Joi.boolean(),
-    //   createdBy:Joi.string(),
-    //   updatedBy:Joi.string(),
-    //   paymentTermId:Joi.string().uuid(),
-    //   summaryTask:Joi.array(),
-    //   projectId: Joi.string().uuid(),
-    //   plannedStart: Joi.date(),
-    //   plannedFinish: Joi.date(),
-    //   startVariance: Joi.number(),
-    //   finishVariance: Joi.number(),
-    //   actualStart: Joi.date(),
-    //   actualFinish: Joi.date(),
-    //   createdAt:Joi.date(),
-    //   updatedAt:Joi.date(),
-    //   hasCheckList:Joi.boolean(),
-    //   isEvaluted:Joi.boolean(),
-    //   isSendToDOO:Joi.boolean(),
-
-    // })
+    .keys({
+      id: Joi.string().uuid().allow(null),
+      name: Joi.string().allow(null),
+      weight: Joi.number().allow(null),
+      status: Joi.boolean().allow(null),
+      createdBy: Joi.string().allow(null),
+      updatedBy: Joi.string().allow(null),
+      paymentTermId: Joi.string().uuid().allow(null),
+      summaryTask: Joi.array().allow(null),
+      projectId: Joi.string().uuid().allow(null),
+      plannedStart: Joi.date().allow(null),
+      plannedFinish: Joi.date().allow(null),
+      startVariance: Joi.number().allow(null),
+      finishVariance: Joi.number().allow(null),
+      actualStart: Joi.date().allow(null),
+      actualFinish: Joi.date().allow(null),
+      createdAt: Joi.date().allow(null),
+      updatedAt: Joi.date().allow(null),
+      hasCheckList: Joi.boolean().allow(null),
+      isEvaluted: Joi.boolean().allow(null),
+      isSendToDOO: Joi.boolean().allow(null),
+    })
     .min(1),
 };
 const updateMilestoneVariance = {
-  body: Joi.array().items(
-    Joi.object({
-      id: Joi.string().uuid().required(),
-      plannedStart: Joi.date(),
-      plannedFinish: Joi.date(),
-      startVariance: Joi.number(),
-      finishVariance: Joi.number(),
-      actualStart: Joi.date(),
-      actualFinish: Joi.date(),
-    })
-  ).min(1),
+  body: Joi.array()
+    .items(
+      Joi.object({
+        id: Joi.string().uuid().required(),
+        plannedStart: Joi.date(),
+        plannedFinish: Joi.date(),
+        startVariance: Joi.number(),
+        finishVariance: Joi.number(),
+        actualStart: Joi.date(),
+        actualFinish: Joi.date(),
+      })
+    )
+    .min(1),
 };
 const deleteMilestone = {
   params: Joi.object().keys({
@@ -81,5 +82,5 @@ module.exports = {
   getByProject,
   updateMilestone,
   deleteMilestone,
-  updateMilestoneVariance
+  updateMilestoneVariance,
 };
