37e79bb0b8e2d5ca4360149a80fcf0872e158a9a
Selaml <selamawitlb@gmail.com>
2024-02-14 16:04:42 +0300

    update master schedule

--- src/routes/v1/monthlyBudget.route.js
+++ src/routes/v1/monthlyBudget.route.js
@@ -6,13 +6,15 @@ const { route } = require('./risk.route');
 
 const router = express.Router();
 
-router.route('/').post(monthlyBudgetController.createMonthlyBudget).get(monthlyBudgetController.getMonthlyBudget);
-
+router.route('/').post(validate(monthlyBudgetValidation.addMonthlyBudget), monthlyBudgetController.createMonthlyBudget).get(monthlyBudgetController.getMonthlyBudget);
+router.route('/officProject').post(monthlyBudgetController.createOfficeMonthlyBudget)
 router.route('/month').get(monthlyBudgetController.getMonthlyBudgetByMonth);
 router.route('/month/project').get(monthlyBudgetController.getMonthlyBudgetByMonthGroupedByProject);
 router.route('/month/officProject').get(monthlyBudgetController.getMonthlyBudgetByMonthGroupedByProjectOfficeProject);
 
 router.route('/:id').patch(monthlyBudgetController.updateMonthlyBudget);
+
+router.route('/officProject:id').patch(monthlyBudgetController.updateOfficeMonthlyBudget);
 router.route('/officProject/:projectId').get(monthlyBudgetController.getMonthlyBudgetByProject);
 
 module.exports = router;
