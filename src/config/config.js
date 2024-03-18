d4334a52a8cb4675ec92955fb0587ed07e3ec857
Abeselom Gebrekidan <abeselomgebrekidan12@gmail.com>
2024-03-18 11:17:23 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -18,6 +18,7 @@ const envVarsSchema = Joi.object()
     PG_CONN_TIMEOUT: Joi.number().required().description('Postgress Connection timeout is required!'),
 
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
+    RABBITMQ_URL: Joi.string().required().description('Rabbit mq url is required!'),
     // JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     // JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
     // JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
@@ -71,4 +72,5 @@ module.exports = {
     },
     from: envVars.EMAIL_FROM,
   },
+  rabbitmqUrl: envVars.RABBITMQ_URL
 };
